d13e33f481c1474a69051bded28a218d8392c617
HFsa-RaShid <hafsa.cse7.bu@gmail.com>
2024-06-19 12:50:37 +0600

    use pagination in manageNotes page

--- src/HOOKS/useNotes.jsx
+++ src/HOOKS/useNotes.jsx
@@ -1,12 +1,31 @@
+// import { useQuery } from "@tanstack/react-query";
+// import useAxiosPublic from "./useAxiosPublic";
+
+// const useNotes = (email) => {
+//     const axiosPublic = useAxiosPublic();
+//     const { refetch, data: noteData = null, isLoading, isError } = useQuery({
+//         queryKey: ['noteData', email],
+//         queryFn: async () => {
+//             const res = await axiosPublic.get(`/notes?userEmail=${email}`);
+//             return res.data;
+//         }
+//     });
+//     return { noteData, refetch, isLoading, isError };
+// };
+
+// export default useNotes;
+
+
+
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "./useAxiosPublic";
 
-const useNotes = (email) => {
+const useNotes = (email, page, limit) => {
     const axiosPublic = useAxiosPublic();
     const { refetch, data: noteData = null, isLoading, isError } = useQuery({
-        queryKey: ['noteData', email],
+        queryKey: ['noteData', email, page, limit],
         queryFn: async () => {
-            const res = await axiosPublic.get(`/notes?userEmail=${email}`);
+            const res = await axiosPublic.get(`/notes?userEmail=${email}&page=${page}&limit=${limit}`);
             return res.data;
         }
     });
@@ -14,6 +33,3 @@ const useNotes = (email) => {
 };
 
 export default useNotes;
-
-
-
